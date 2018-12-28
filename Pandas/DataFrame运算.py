3fba709b78a8040b944dc33a1b8950e2fd3b58e9
XiaoJiaoYaZi <35963340+XiaoJiaoYaZi@users.noreply.github.com>
2018-12-28 13:39:21 +0800

    清洗线上数据，pandas写入csv

--- Pandas/DataFrame运算.py
+++ Pandas/DataFrame运算.py
@@ -1,3 +1,17 @@
 import pandas as pd
 import numpy as np
+import time
+data = pd.DataFrame([1,2,3,4,5,'123\r\n123'])
+data = data.T
+# start = time.time()
+# data.to_csv('test.csv',mode='a',index = False,header = False)
+# data.to_csv('test.csv',mode='a',index = False,header = False)
+# data.to_csv('test.csv',mode='a',index = False,header = False)
+# print(time.time()-start)
+import xlwt
+
+import csv
+
+with open('test.csv','a') as f:
+    data.to_csv(f, mode='a', index=False, header=False,line_terminator = '')
 
